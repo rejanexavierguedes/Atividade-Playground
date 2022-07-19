421ab3474234c292b4c1b29ac0b4fd6413609342
Rejane Guedes <rejane.cristian@gmail.com>
2022-07-18 21:13:45 -0300

    AulaPraticaPlayground

---
+++ playground3.js
@@ -0,0 +1,15 @@
+function somaNmeros(arr){
+    return arr.reduce(function (prev, current){
+        
+        console.log({prev});
+        console.log({current});
+        return prev + current;
+
+    });
+
+}
+
+    const arr = [1, 2];
+
+    console.log(somaNmeros(arr));
+    
